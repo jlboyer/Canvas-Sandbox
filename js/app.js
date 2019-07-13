6c0063e5b750ae58c829e1ec65a177eecc85433e
jlboyer <jlboyer1@gmail.com>
2019-07-13 14:59:53 -0500

    dynamic resize of canvas

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
-$("canvas").width("100vw");
-$("canvas").height("100vh");
+const game = {
+  ctx: document.getElementById("canvas").getContext("2d"),
+  initialize() {
+    game.resizeCanvas()
+    window.addEventListener("resize", game.resizeCanvas, false);
+  },
+  resizeCanvas(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    game.drawStuff()
+  },
+  drawStuff() {
+    // do your drawing stuff here
+    game.ctx.beginPath();
+    game.ctx.arc(100, 100, 20, 0, 2 * Math.PI);
+    game.ctx.fillStyle = "white";
+    game.ctx.fill();
+  },
+};
+
+game.initialize()
