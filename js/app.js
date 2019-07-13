ee6a2e588b37a5c9e877ea28f2bb7e4a684429a4
jlboyer <jlboyer1@gmail.com>
2019-07-13 17:51:33 -0500

    translate based on scroll and mouse postion

--- js/app.js
+++ js/app.js
@@ -1,25 +1,44 @@
 const game = {
-  canvas: $("#canvas"),
+  canvas: document.getElementById("canvas"),
   ctx: this.canvas.getContext("2d"),
   mouse: {x: 0,y: 0},
   delta: {x: 0,y: 0},
-  canvas: {x: 1400, y: 800},
+  canvas: {width: 1400, height: 800},
+
+
   initialize() {
-  
     game.animate()
-
-    $("window").resize(game.animate.bind(this));
-
-    $("#canvas").mousemove( e => { 
-      game.mouse.x = e.pageX
-      game.mouse.y = e.clientY
+    //ON WINDOW RESIZE EVENT CALL ANIMATION FUNCTION AGAIN----------
+    window.addEventListener("resize", game.animate.bind(this))
+    //ON MOUSE MOVE UPDATE COORDINATES IN THE GAME OBJECT-----------
+    document.onmousemove = (evt) => {
+      game.mouse.x = this.getMousePos(canvas, evt).x
+      game.mouse.y = this.getMousePos(canvas, evt).y
+    }
+    //--------------------------------------------------------------
+    
+    window.addEventListener('wheel', (evt) => {
+      if (evt.ctrlKey) {
+        scale -= e.deltaY * 0.01;
+      } else {
+        game.mouse.x-= evt.deltaX * 2;
+        game.mouse.y -= evt.deltaY * 2;
+      }
     });
-
+    
+  },
+  getMousePos(canvas, evt) {
+    let rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    }
   },
   clearCanvas() {
     game.ctx.clearRect(0, 0, canvas.width, canvas.height)  
   },
   animate() {
+    //IN EVENT OF RESIZE 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
@@ -28,15 +47,8 @@ const game = {
     game.ctx.arc(game.mouse.x, game.mouse.y, 20, 0, 2 * Math.PI);
     game.ctx.fillStyle = "white";
     game.ctx.fill();
-
+    //ANIMATION (RECURSIVE FUNCTION CALL) AT 60 FPS
     window.requestAnimationFrame(game.animate)
-  },
-  getMousePos(canvas, evt) {
-    let rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    }
   }
 };
 
